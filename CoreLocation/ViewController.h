57146e9db6c6e94c4da9786775f616671f73d1a3
ipodishima <marian.paul2@gmail.com>
2012-03-08 23:22:11 -0500

    Initial Commit

---
+++ CoreLocation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreLocation
+//
+//  Created by Marian PAUL on 08/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
